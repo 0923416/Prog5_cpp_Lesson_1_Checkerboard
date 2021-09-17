b7980d2b93cee45fb7008102ac5821961e8a9f76
Rens-Surface <rvanvendeloo@gressive.nl>
2021-09-17 18:18:05 +0200

    First init of GIT Repo

---
+++ cmake-build-debug/CMakeFiles/Huiswerk_Les_1__Checkerboard_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Huiswerk_Les_1__Checkerboard_.dir/main.cpp.obj"
+  "Huiswerk_Les_1__Checkerboard_.exe"
+  "Huiswerk_Les_1__Checkerboard_.exe.manifest"
+  "Huiswerk_Les_1__Checkerboard_.pdb"
+  "libHuiswerk_Les_1__Checkerboard_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Huiswerk_Les_1__Checkerboard_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Huiswerk_Les_1__Checkerboard_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Huiswerk_Les_1__Checkerboard_.
+# This may be replaced when dependencies are built.
