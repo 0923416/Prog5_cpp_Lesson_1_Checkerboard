776641e23d9276b3935f90349265571340d212e9
Rens-Surface <rvanvendeloo@gressive.nl>
2021-09-17 20:06:17 +0200

    Added variables

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <vector>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "This assignment is made by Rens van Vendeloo (0923416) " << std::endl;
+    std::cout << "Made with Clion - Written in C++23" << std::endl << std::endl ;
+
+    std::vector<std::vector<int>> renderBuffer; //2 dimensional array contains brightness values
+
+    int circleX = 0;
+    int circleY = 0;
+    int circleR = 10;
+    int horizontalResolution = 100;
+    int verticalResolution = 100;
+
+    //calculate position of circle on screen canvas
+
+
+
+
+
     return 0;
 }
